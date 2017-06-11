6f1a67c7db179b44a1b23591c7e3dcb72f4565c1
march-dave <ysangy@gmail.com>
2017-06-11 12:42:20 -0400

    up to date

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const restService = express();
 
-const axios = require('axios');
+var axios = require('axios');
 
 restService.use(bodyParser.urlencoded({
     extended: true
@@ -20,33 +20,78 @@ function getTrendingNews() {
        'Content-Type': 'application/json',
      }})
     .then(function(response) {
-
-      console.log('response');
-      console.log(response.data[0].firstname);
-
        TrendingNews = response.data;
      })
-
-   // axios.get('https://chooseapi.mybluemix.net/api/people')
-   //    .then(function (res) {
-   //      app.ask(res.body[0].firstname);
-   //    })
 }
+
+var index_localNews = 0;
+
+function returnOneLocalNews(){
+  index_localNews = index_localNews + 1;
+  return TrendingNews[index_localNews % TrendingNews.length];
+}
+
+var index_EventNews = 0;
+
+var EventNews = [];
+
+function returnOneEventNews(){
+  index_EventNews = index_EventNews + 1;
+  return EventNews[index_EventNews % EventNews.length];
+}
+
+
+
+var curent_subject = '';
 
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     const speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "I don't understand Speak again.";
+    let res_str;
+
+   if (speech==="local news"){
+      curent_path = "general"
+      res_str =  "Here is some trending news. " + returnOneLocalNews() + "Say next to continue";
+    }
+    else if (speech==="next" && curent_path === "general"){
+      res_str = returnOneLocalNews();
+    }
+    else if (speech==="repeat" && curent_path === "general"){
+      index_localNews = index_localNews -1 ;
+      res_str = returnOneLocalNews();
+    }
+    else if (speech === "topic"){
+      curent_path = "topic";
+      res_str = "What topic do you want?";
+    }
+    else if (speech === "events"){
+      curent_path = "events";
+      res_str = "Here is some news about local events. " + returnOneEventNews();
+    }
+
+   else if (speech==="next" && curent_path === "events"){
+       res_str = returnOneEventNews();
+    }
+    else if (speech==="repeat" && curent_path === "events"){
+      index_EventNews = index_EventNews - 1;
+       res_str = returnOneEventNews();
+    }
+    else {
+      res_str = "I don't understand Speak again.";
+    }
+
 
    return res.json({
-        speech: TrendingNews[0].firstname,
-        displayText: TrendingNews[0].firstname,
+        speech: res_str,
+        displayText: res_str,
         source: ''
     });
 });
 
 
+
 restService.listen((process.env.PORT || 8000), function() {
-   getTrendingNews();
+    getTrendingNews();
     console.log("Server up and listening");
 });
